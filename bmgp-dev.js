620245fed471e1fbf2259ccac239f4b9a415829a
Code Monkey King <freesoft.for.people@gmail.com>
2024-06-28 10:09:20 +0300

    cleaned up code, final tests

--- bmgp-dev.js
+++ bmgp-dev.js
@@ -1,9 +1,18 @@
-// Init board canvas
+/*****************************************\
+  =======================================
+ 
+         Bare Minimum Go Program
+
+                    by
+
+             Code Monkey King
+
+  =======================================
+\*****************************************/
+
+// DATA
 const canvas = document.getElementById('gobang');
 const ctx = canvas.getContext('2d');
-canvas.addEventListener('click', userInput);
-
-// Game state
 const EMPTY = 0
 const BLACK = 1
 const WHITE = 2
@@ -21,65 +30,11 @@ var points_count = [];
 var ko = EMPTY;
 var bestMove = EMPTY;
 var cell = canvas.width / size;
-
-// Change board size
 var selectSize = document.getElementById("size");
-selectSize.addEventListener("change", function() {
-  size = parseInt(selectSize.value);
-  cell = canvas.width / size;
-  initBoard();
-  drawBoard();
-  side = BLACK;
-  ko = EMPTY;
-});
+var attempts = 0;
 
-function userInput(event) {
-  let rect = canvas.getBoundingClientRect();
-  let mouseX = event.clientX - rect.left;
-  let mouseY = event.clientY - rect.top;
-  let col = Math.floor(mouseX / cell);
-  let row = Math.floor(mouseY / cell);
-  let sq = row * size + col;
-  if (board[sq]) return;
-  if (!setStone(sq, side, true)) return;
-  drawBoard();
-  setTimeout(function() { play(4); }, 10);
-}
-
-// Init board
-function initBoard() {
-  for (let sq = 0; sq < size ** 2; sq++) {
-    switch (true) {
-      case (sq < size):
-      case (sq >= (size ** 2 - size)):
-      case (!(sq % size)):
-        board[sq] = OFFBOARD;
-        board[sq-1] = OFFBOARD;
-        break;
-      default: board[sq] = 0;
-    }
-  }
-}
-
-// Print position to console
-function printPosition() {
-  let position = "";
-  for (let sq = 0; sq < size ** 2; sq++) {
-    position += (!(sq % size)) ? "\n" : "";
-    position += board[sq] + " ";
-  };console.log(position);
-  console.log("Side to move: " + (side == 1 ? "black" : "white"));
-}
-
-// Print score
-function updateScore() {
-  let pts = score();
-  let element = document.getElementById("score");
-  element.innerHTML = "Black " + pts[BLACK] + ", White " + pts[WHITE] + ", Empty " + pts[EMPTY];
-}
-
-// Print board
-function drawBoard() {
+// GUI
+function drawBoard() { /* Render board to screen */
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath();
   for (let i = 1; i < size-1; i++) {
@@ -115,8 +70,126 @@ function drawBoard() {
   }
 }
 
-// Put stone on board
-function setStone(sq, color, user) {
+function userInput(event) { /* Handle user input */
+  let rect = canvas.getBoundingClientRect();
+  let mouseX = event.clientX - rect.left;
+  let mouseY = event.clientY - rect.top;
+  let col = Math.floor(mouseX / cell);
+  let row = Math.floor(mouseY / cell);
+  let sq = row * size + col;
+  if (board[sq]) return;
+  if (!setStone(sq, side, true)) return;
+  drawBoard();
+  setTimeout(function() { play(4); }, 10);
+}
+
+function territory(sq) { /* Count territory, returns [side, points]*/
+  stone = board[sq];
+  if (stone == OFFBOARD) return OFFBOARD;
+  if (stone == EMPTY) {
+    block.push(sq);
+    points_count.push(sq);
+    board[sq] |= MARKER;
+    for (let offset of [1, -1, size, -size]) territory(sq+offset);
+  } else if (stone != MARKER) {
+    points_side.push(stone);
+  } if (!points_side.length) return [EMPTY, points_count.length];
+  else if (points_side.every((element) => element == points_side[0]))
+    return [points_side[0], points_count.length];
+  else return [EMPTY, points_count.length];
+}
+
+function score() { /* Scores game, returns points [empty, black, white]*/
+  let scorePosition = [0, 0, 0];
+  for (let sq = 0; sq < size ** 2; sq++) {
+    if (board[sq]) continue;
+    let result = territory(sq);
+    scorePosition[result[0]] += result[1];
+    points_side = [];
+    points_count = [];
+  } restoreBoard();
+  let prisoners = evaluate();
+  if (prisoners > 0) scorePosition[BLACK] += prisoners;
+  else if (prisoners < 0) scorePosition[WHITE] += Math.abs(prisoners);
+  scorePosition[WHITE] += 7.5;
+  return scorePosition;
+}
+
+function updateScore() { /* Render score on screen */
+  let pts = score();
+  let element = document.getElementById("score");
+  element.innerHTML = "Black " + pts[BLACK] + ", White " + pts[WHITE] + ", Empty " + pts[EMPTY];
+}
+
+// ENGINE
+function initBoard() { /* Empty board, set offboard squares */
+  for (let sq = 0; sq < size ** 2; sq++) {
+    switch (true) {
+      case (sq < size):
+      case (sq >= (size ** 2 - size)):
+      case (!(sq % size)):
+        board[sq] = OFFBOARD;
+        board[sq-1] = OFFBOARD;
+        break;
+      default: board[sq] = 0;
+    }
+  }
+}
+
+function inEye(sq, offset) { /* Check if sqaure is in diamond shape */
+  let count = 0;
+  for (let dir of [1, -1, size, -size]) {
+    if (board[sq+offset+dir] == side ||
+      board[sq+offset+dir] == (3-side) ||
+      board[sq+offset+dir] == OFFBOARD)
+      count++;
+  };if (count == 4) return 1;
+  else return 0;
+}
+
+function clearBlock() { /* Erase stones when captured */
+  if (block.length == 1) {
+    let count = 0;
+    for (let sq of [size+1, size-1, -size+1, -size-1]) {
+      if (board[block[0] + sq] == EMPTY) count += 1;
+    };if (count) ko = block[0];
+  };for (let i = 0; i < block.length; i++)
+    board[block[i]] = EMPTY;
+}
+
+function captures(color) { /* Handle captured stones */
+  for (let sq = 0; sq < size ** 2; sq++) {
+    let stone = board[sq];
+    if (stone == OFFBOARD) continue;
+    if (stone & color) {
+      count(sq, color);
+      if (liberties.length == 0) clearBlock();
+      restoreBoard()
+    }
+  }
+}
+
+function count(sq, color) { /* Count group liberties */
+  stone = board[sq];
+  if (stone == OFFBOARD) return;
+  if (stone && (stone & color) && (stone & MARKER) == 0) {
+    block.push(sq);
+    board[sq] |= MARKER;
+    for (let offset of [1, -1, size, -size]) count(sq+offset, color);
+  } else if (stone == EMPTY) {
+    board[sq] |= LIBERTY;
+    liberties.push(sq);
+  }
+}
+
+function restoreBoard() { /* Remove group markers */
+  block = []; liberties = []; points_side = [];
+  for (let sq = 0; sq < size ** 2; sq++) {
+    if (board[sq] != OFFBOARD) board[sq] &= 3;
+  }
+}
+
+function setStone(sq, color, user) { /* Place stone on board */
   if (board[sq] != EMPTY) {
     if (user) alert("Illegal move!");
     return false;
@@ -140,101 +213,18 @@ function setStone(sq, color, user) {
   return true;
 }
 
-// Handle captures
-function captures(color) {
+function getUrgentMoves() { /* Get escape squares of groups with less than 3 liberties */
+  let urgent = [];
   for (let sq = 0; sq < size ** 2; sq++) {
-    let stone = board[sq];
-    if (stone == OFFBOARD) continue;
-    if (stone & color) {
-      count(sq, color);
-      if (liberties.length == 0) clearBlock();
-      restoreBoard()
-    }
-  }
+    count(sq, board[sq]);
+    if (liberties.length < 3) {
+      if (board[sq]-MARKER == BLACK) for (let sq of liberties) urgent.push(sq);
+      if (board[sq]-MARKER == WHITE) for (let sq of liberties) urgent.push(sq);
+    };restoreBoard();
+  };return urgent;
 }
 
-// Count liberties, save stone group coords
-function count(sq, color) {
-  stone = board[sq];
-  if (stone == OFFBOARD) return;
-  if (stone && (stone & color) && (stone & MARKER) == 0) {
-    block.push(sq);
-    board[sq] |= MARKER;
-    count(sq - size, color);
-    count(sq - 1, color);
-    count(sq + size, color);
-    count(sq + 1, color);
-  } else if (stone == EMPTY) {
-    board[sq] |= LIBERTY;
-    liberties.push(sq);
-  }
-}
-
-// Restore the board after counting stones
-function restoreBoard() {
-  clearGroups();
-  for (let sq = 0; sq < size ** 2; sq++) {
-    if (board[sq] != OFFBOARD) board[sq] &= 3;
-  }
-}
-
-// Remove captured stones
-function clearBlock() {
-  if (block.length == 1) {
-    let count = 0;
-    for (let sq of [size+1, size-1, -size+1, -size-1]) {
-      if (board[block[0] + sq] == EMPTY) count += 1;
-    };if (count) ko = block[0];
-  };for (let i = 0; i < block.length; i++)
-    board[block[i]] = EMPTY;
-}
-
-// Clear groups
-function clearGroups() {
-  block = [];
-  liberties = [];
-  points_side = [];
-}
-
-// Count territory
-function territory(sq) {
-  stone = board[sq];
-  if (stone == OFFBOARD) return OFFBOARD;
-  if (stone == EMPTY) {
-    block.push(sq);
-    points_count.push(sq);
-    board[sq] |= MARKER;
-    territory(sq - size);
-    territory(sq - 1);
-    territory(sq + size);
-    territory(sq + 1);
-  } else if (stone != MARKER) {
-    points_side.push(stone);
-  } if (!points_side.length) return [EMPTY, points_count.length];
-  else if (points_side.every((element) => element == points_side[0]))
-    return [points_side[0], points_count.length];
-  else return [EMPTY, points_count.length];
-}
-
-// Calculate score for sides
-function score() {
-  let scorePosition = [0, 0, 0];
-  for (let sq = 0; sq < size ** 2; sq++) {
-    if (board[sq]) continue;
-    let result = territory(sq);
-    scorePosition[result[0]] += result[1];
-    points_side = [];
-    points_count = [];
-  } restoreBoard();
-  let prisoners = evaluate();
-  if (prisoners > 0) scorePosition[BLACK] += prisoners;
-  else if (prisoners < 0) scorePosition[WHITE] += Math.abs(prisoners);
-  scorePosition[WHITE] += 7.5;
-  return scorePosition;
-}
-
-// Evaluate position
-function evaluate() {
+function evaluate() { /* Count captures stones difference */
   let eval = 0;
   let blackStones = 0;
   let whiteStones = 0;
@@ -248,32 +238,7 @@ function evaluate() {
   return (side == BLACK) ? eval : -eval;
 }
 
-// Find biggest group to capture
-function isAtari() {
-  let bestAtari = [-1, -1];
-  for (let sq = 0; sq < size ** 2; sq++) {
-    count(sq, board[sq]);
-    if (liberties.length == 1) {
-      if (board[sq]-MARKER == 3-side && block.length > bestAtari[1])
-        bestAtari = [liberties[0], block.length];
-    };restoreBoard();
-  };return bestAtari[0];
-}
-
-// Find fighting moves
-function getUrgentMoves() {
-  let urgent = [];
-  for (let sq = 0; sq < size ** 2; sq++) {
-    count(sq, board[sq]);
-    if (liberties.length < 3) {
-      if (board[sq]-MARKER == BLACK) for (let sq of liberties) urgent.push(sq);
-      if (board[sq]-MARKER == WHITE) for (let sq of liberties) urgent.push(sq);
-    };restoreBoard();
-  };return urgent;
-}
-
-// Find best move
-function search(depth) {
+function search(depth) { /* Recursively search fighting moves */
   if (!depth) return evaluate();
   let bestScore = -10000;
   for (let sq of getUrgentMoves()) {
@@ -294,28 +259,7 @@ function search(depth) {
   };return bestScore;
 }
 
-// Used to allow snapbacks
-function inEye(sq, offset) {
-  let count = 0;
-  for (let dir of [1, -1, size, -size]) {
-    if (board[sq+offset+dir] == side ||
-      board[sq+offset+dir] == (3-side) ||
-      board[sq+offset+dir] == OFFBOARD)
-      count++;
-  };if (count == 4) return 1;
-  else return 0;
-}
-
-// Used to attach randomly
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  };return array;
-}
-
-// Play away
-function tenuki() {
+function tenuki() { /* Play away when no urgent moves */
   const corners = [(4*size+4), (4*size+(size-5)), ((size-5)*size+4), ((size-5)*size+(size-5))];
   const sides = [((size-1)/2*size+3), (3*size+(size-1)/2), ((size-1)/2*size+(size-4)), ((size-4)*size+(size-1)/2)];
   for (let sq of corners) {
@@ -323,12 +267,12 @@ function tenuki() {
       if (inEye(sq, 0)) break;
       else return sq;
     }
-  };if (size > 11) {
-    for (let sq of sides) if (board[sq] == EMPTY) {
-      if (inEye(sq, 0)) break;
-      else return sq;
-    }
-  };if (score()[EMPTY]) {
+  }
+  for (let sq of sides) if (board[sq] == EMPTY) {
+    if (inEye(sq, 0)) break;
+    else return sq;
+  }
+  if (score()[EMPTY]) {
     let indexes = Array.from({length: size ** 2}, (_, i) => i);
     let shuffledIndexes = shuffle(indexes);
     for (let sq of shuffledIndexes) {
@@ -347,9 +291,18 @@ function tenuki() {
   }
 }
 
-// Engine plays move
-var attempts = 0;
-function play(depth) {
+function isAtari() { /* Find capture square of the biggest group */
+  let bestAtari = [-1, -1];
+  for (let sq = 0; sq < size ** 2; sq++) {
+    count(sq, board[sq]);
+    if (liberties.length == 1) {
+      if (board[sq]-MARKER == 3-side && block.length > bestAtari[1])
+        bestAtari = [liberties[0], block.length];
+    };restoreBoard();
+  };return bestAtari[0];
+}
+
+function play(depth) { /* Engine plays a move */
   let eval = 0;
   let bestQuick = isAtari();
   if (bestQuick >= 0 && bestQuick != ko) bestMove = bestQuick;
@@ -359,6 +312,7 @@ function play(depth) {
   };let oldSide = side;
   if (!setStone(bestMove, side, false)) {
     if (attempts > 10) {
+      console.log("attempts: " + attempts);
       side = 3 - side;
       updateScore();
       let empty = score()[EMPTY];
@@ -377,6 +331,30 @@ function play(depth) {
   let scorePosition = score();
 }
 
-// Main
-initBoard();
-drawBoard();
+// MISC
+function shuffle(array) { /* Used to randomize attach moves when tenuki */
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  };return array;
+}
+
+function printPosition() { /* Debug board array */
+  let position = "";
+  for (let sq = 0; sq < size ** 2; sq++) {
+    position += (!(sq % size)) ? "\n" : "";
+    position += board[sq] + " ";
+  };console.log(position);
+  console.log("Side to move: " + (side == 1 ? "black" : "white"));
+}
+
+// MAIN
+canvas.addEventListener('click', userInput);
+selectSize.addEventListener("change", function() {
+  size = parseInt(selectSize.value);
+  cell = canvas.width / size;
+  initBoard();
+  drawBoard();
+  side = BLACK;
+  ko = EMPTY;
+}); initBoard(); drawBoard();
